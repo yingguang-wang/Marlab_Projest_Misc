837d305ff68af4bb87527bc5aa9f1cfe4c9f779f
yingguang-wang <97671348+yingguang-wang@users.noreply.github.com>
2023-11-13 08:55:23 +0800

    Update project_2023_11_13_P75_1.m

--- 动力学建模控制仿真/Project_2023_11_13/project_2023_11_13_P75_1.m
+++ 动力学建模控制仿真/Project_2023_11_13/project_2023_11_13_P75_1.m
@@ -1,13 +1,14 @@
 num = [1];
+%杈炬垚鐩殑鏁堟灉,den = [1,0.05,1]
 den = [1,0.5,1];
 w = 0.05:0.001:0.5 * pi;
 Gw = polyval(num, j * w)./polyval(den, j * w);
 mag = real(Gw);
 theta = imag(Gw);
 subplot(2,1,1),plot(w,mag);
-grid,title("频率特性的实部");
+grid,title("棰戠巼鐗规�х殑瀹為儴");
 ylabel('G');
 subplot(2,1,2),plot(w,theta);
-grid,title('频率特性的虚部');
+grid,title('棰戠巼鐗规�х殑铏氶儴');
 xlabel('\omega(rad/s)');
 ylabel('G');
